1c9e515b0e9ea5ac4d17799ebf84c304dde0faf3
jiao.li <jiao.li@2339.com>
2017-02-13 19:05:54 +0800

    Game id bug

--- src/main/groovy/crontab/st/UpdateUserAndLive.groovy
+++ src/main/groovy/crontab/st/UpdateUserAndLive.groovy
@@ -208,7 +208,7 @@ class UpdateUserAndLive {
     }
 
     def liveCheck() {
-        rooms.find(new BasicDBObject('live', true), new BasicDBObject(live_id: 1, timestamp: 1, type: 1, mic_user: 1, mic_live_id: 1, live_type: 1)).toArray().each { room ->
+        rooms.find(new BasicDBObject('live', true), new BasicDBObject(live_id: 1, timestamp: 1, type: 1, game_id: 1, live_type: 1)).toArray().each { room ->
             def roomId = room.get("_id")
             String live_id = room.get("live_id")
             String game_id = room.get("game_id")
@@ -227,7 +227,7 @@ class UpdateUserAndLive {
                         logRoomEdit.save(new BasicDBObject(type: 'live_off', room: roomId, data: live_id, live_type: live_type, status: 'LiveStatusCheck', timestamp: l))
                     }
                     finally {
-                        def set = new BasicDBObject(live: false, live_id: '', timestamp: l, live_end_time: l, mic_user: 0, mic_live_id: '', position: null, pull_urls: null)
+                        def set = new BasicDBObject(live: false, live_id: '', timestamp: l, live_end_time: l, game_id: '', position: null, pull_urls: null)
                         //家族房间
                         if (type.equals(2)) {
                             set.append('xy_star_id', null)
