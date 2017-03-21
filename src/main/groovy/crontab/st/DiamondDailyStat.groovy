234aec7b7649b72ade923c1c1829c0aed15b1062
monkey <monkey.d.luffer@gmail.com>
2017-03-21 18:27:42 +0800

    钻石出入账脚本统计

--- src/main/groovy/crontab/st/DiamondDailyStat.groovy
+++ src/main/groovy/crontab/st/DiamondDailyStat.groovy
@@ -15,7 +15,7 @@ import com.mongodb.MongoURI
 /**
  * 关于钻石的统计
  */
-class DiamondStat {
+class DiamondDailyStat {
     static Properties props = null;
     static String profilepath = "/empty/crontab/db.properties";
 
@@ -33,7 +33,8 @@ class DiamondStat {
 
     static mongo = new Mongo(new MongoURI(getProperties('mongo.uri', 'mongodb://192.168.31.231:20000,192.168.31.236:20000,192.168.31.231:20001/?w=1&slaveok=true') as String))
     static DBCollection diamond_logs = mongo.getDB('xy_admin').getCollection('diamond_logs')
-    static DBCollection diamond_stat = mongo.getDB('xy_admin').getCollection('diamond_stat')
+    static DBCollection diamond_cost_logs = mongo.getDB('xy_admin').getCollection('diamond_cost_logs')
+    static DBCollection diamond_stat = mongo.getDB('xy_admin').getCollection('diamond_daily_stat')
     static DAY_MILLON = 24 * 3600 * 1000L
     static long zeroMill = new Date().clearTime().getTime()
     static Long yesTday = zeroMill - DAY_MILLON
@@ -47,26 +48,59 @@ class DiamondStat {
      * 统计钻石的入账，出账，总账
      */
     private static void statics_diamond() {
+        //本日期初结余=昨日期末结余
+        def begin_surplus = lastDaySurplus(yesTday)
         def inc_total = 0
         def desc_total = 0
-        def total = 0
+        def inc_detail = new HashMap<String, Long>()
+        def desc_detail = new HashMap<String, Long>()
         def query = $$('timestamp': getTimeBetween())
-        def cursors = diamond_logs.find(query).batchSize(5000)
-        while (cursors.hasNext()) {
-            def obj = cursors.next()
-            def cost = obj['cost'] as Long
-            if (cost > 0) {
-                inc_total += 1
-            } else {
-                desc_total += 1
-            }
-            total += 1
-        }
-        def curr_date = new Date(yesTday - day * DAY_MILLON)
-        def myId = "diamond_stat_" + curr_date.format("yyyyMMdd")
+        def diamond_cursors = diamond_logs.find(query).batchSize(5000)
+        def diamond_cost_cursors = diamond_cost_logs.find(query).batchSize(5000)
 
-        def row = $$('_id': myId, 'inc_total': inc_total, 'desc_total': desc_total, 'total': total, 'timestamp': curr_date.getTime())
+        // 加币统计
+        while (diamond_cursors.hasNext()) {
+            def obj = diamond_cursors.next()
+            def diamond_count = obj['diamond_count'] as Long
+            def type = obj['type'] as String
+            inc_total += diamond_count
+            def current_type_add_diamond = inc_detail.containsKey(type) ? inc_detail[type] : 0L
+            current_type_add_diamond += diamond_count
+            inc_detail.put(type, current_type_add_diamond)
+            println("add_diamond_total is   ${current_type_add_diamond}")
+        }
+
+        // 消费统计
+        while (diamond_cost_cursors.hasNext()) {
+            def obj = diamond_cost_cursors.next()
+            def diamond_count = obj['diamond_count'] as Long
+            def type = obj['type'] as String
+            desc_total += diamond_count
+            def current_type_minus_total = desc_detail.containsKey(type) ? desc_detail[type] : 0L
+            current_type_minus_total += diamond_count
+            desc_detail.put(type, current_type_minus_total)
+            println("current_type_minus_total is   ${current_type_minus_total}")
+        }
+
+        def total = inc_total - desc_total
+        def curr_date = new Date(yesTday - day * DAY_MILLON)
+        def myId = curr_date.format("yyyyMMdd") + "_diamond_daily_stat"
+
+        def row = $$('_id': myId, 'inc_total': inc_total, 'desc_total': desc_total, 'total': total, 'timestamp': curr_date.getTime(),
+                'inc_detail': inc_detail, 'desc_detail': desc_detail, 'begin_surplus': begin_surplus, 'end_surplus': total)
         diamond_stat.save($$(row))
+    }
+
+    /**
+     * 日期末结余
+     * @param begin
+     * @return
+     */
+    static Long lastDaySurplus(Long begin) {
+        long yesterDay = begin - DAY_MILLON
+        String ymd = new Date(yesterDay).format("yyyyMMdd")
+        def last_day = diamond_stat.findOne($$(_id: "${ymd}_diamond_daily_stat".toString()))
+        return (last_day?.get('end_surplus') ?: 0) as Long;
     }
 
     final static Integer day = 0;
@@ -76,7 +110,7 @@ class DiamondStat {
         long begin = l
 
         statics_diamond()
-        println "${new Date().format('yyyy-MM-dd HH:mm:ss')}   ${DiamondStat.class.getSimpleName()},statics_diamond cost  ${System.currentTimeMillis() - l} ms"
+        println "${new Date().format('yyyy-MM-dd HH:mm:ss')}   ${DiamondDailyStat.class.getSimpleName()},statics_diamond cost  ${System.currentTimeMillis() - l} ms"
         Thread.sleep(1000L)
 
         jobFinish(begin);
@@ -87,10 +121,10 @@ class DiamondStat {
      * @return
      */
     private static jobFinish(Long begin) {
-        def timerName = 'DiamondStat'
+        def timerName = 'DiamondDailyStat'
         Long totalCost = System.currentTimeMillis() - begin
         saveTimerLogs(timerName, totalCost)
-        println "${new Date().format('yyyy-MM-dd')}:${DiamondStat.class.getSimpleName()}:finish  cost time:  ${System.currentTimeMillis() - begin} ms"
+        println "${new Date().format('yyyy-MM-dd')}:${DiamondDailyStat.class.getSimpleName()}:finish  cost time:  ${System.currentTimeMillis() - begin} ms"
     }
 
     //落地定时执行的日志
