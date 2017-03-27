f4c38d692d28e8bec71879a79d8edb7878b5f0a1
monkey <monkey.d.luffer@gmail.com>
2017-03-27 09:59:25 +0800

    修改Recovery脚本

--- src/main/groovy/tmp/tmp/Recovery.groovy
+++ src/main/groovy/tmp/tmp/Recovery.groovy
@@ -49,10 +49,11 @@ class Recovery {
     static Long yesTday = zeroMill - DAY_MILLON
 
     static DBCollection coll = mongo.getDB('xy_admin').getCollection('stat_daily')
-    static DBCollection finance_log_DB = mongo.getDB('xy_admin').getCollection('finance_log')
     static DBCollection users = mongo.getDB('xy').getCollection('users')
-    static DBCollection channel_pay_DB = mongo.getDB('xy_admin').getCollection('channel_pay')
     static DBCollection channels = mongo.getDB('xy_admin').getCollection('channels')
+    static DBCollection diamond_logs = mongo.getDB('xy_admin').getCollection('diamond_logs')
+    static DBCollection diamond_cost_logs = mongo.getDB('xy_admin').getCollection('diamond_cost_logs')
+    static DBCollection diamond_dailyReport_stat = mongo.getDB('xy_admin').getCollection('diamond_dailyReport_stat')
 
     /**
      * 充值统计recovery
@@ -60,6 +61,15 @@ class Recovery {
     static pay_statics_recovery(int day) {
         for (int i = 0; i < day; i++) {
             financeStatics(i)
+        }
+    }
+
+    /**
+     * 钻石统计recovery
+     */
+    static diamond_statics_recovery(int day) {
+        for (int i = 0; i < day; i++) {
+            statics_diamond(i)
         }
     }
 
@@ -89,11 +99,11 @@ class Recovery {
             def cny = obj.get('cny') as Double
             def payType = pays[obj.via]
             payType.count.incrementAndGet()
-            if(payType.user.add(obj.user_id) && cny != null){
+            if (payType.user.add(obj.user_id) && cny != null) {
                 // 新增统计android和ios充值情况
                 def userId = obj['user_id'] as Integer
                 def user = users.findOne($$('_id': userId), $$('qd': 1))
-                if(user == null){
+                if (user == null) {
                     return
                 }
                 def qd = user.containsField('qd') ? user['qd'] : 'aiwan_default'
@@ -131,26 +141,93 @@ class Recovery {
                 android_recharge: android_recharge,
                 ios_recharge: ios_recharge,
                 other_recharge: other_recharge,
-                ios_recharge_count:ios_recharge_count,
-                android_recharge_count:android_recharge_count,
-                other_recharge_count:other_recharge_count,
+                ios_recharge_count: ios_recharge_count,
+                android_recharge_count: android_recharge_count,
+                other_recharge_count: other_recharge_count,
                 timestamp: gteMill
         )
         pays.each { String key, PayType type -> obj.put(StringUtils.isBlank(key) ? '' : key.toLowerCase(), type.toMap()) }
 
         coll.save(obj)
+    }
 
+    /**
+     * 统计钻石的入账，出账，总账
+     */
+    private static void statics_diamond(int i) {
+        def gteMill = yesTday - i * DAY_MILLON
+        def YMD = new Date(gteMill).format("yyyyMMdd")
+        def time = [$gte: gteMill, $lt: gteMill + DAY_MILLON]
+        //本日期初结余=昨日期末结余
+        def begin_surplus = lastDaySurplus(gteMill)
+        def inc_total = 0
+        def desc_total = 0
+        def inc_detail = new HashMap<String, Long>()
+        def desc_detail = new HashMap<String, Long>()
+        def query = $$('timestamp': time)
+        def diamond_cursors = diamond_logs.find(query).batchSize(5000)
+        def diamond_cost_cursors = diamond_cost_logs.find(query).batchSize(5000)
+
+        // 加币统计
+        while (diamond_cursors.hasNext()) {
+            def obj = diamond_cursors.next()
+            def diamond_count = obj['diamond_count'] as Long
+            def type = obj['type'] as String
+            inc_total += diamond_count
+            def current_type_add_diamond = inc_detail.containsKey(type) ? inc_detail[type] : 0L
+            current_type_add_diamond += diamond_count
+            inc_detail.put(type, current_type_add_diamond)
+        }
+
+        // 消费统计
+        while (diamond_cost_cursors.hasNext()) {
+            def obj = diamond_cost_cursors.next()
+            def diamond_count = obj['diamond_count'] as Long
+            def type = obj['type'] as String
+            desc_total += diamond_count
+            def current_type_minus_total = desc_detail.containsKey(type) ? desc_detail[type] : 0L
+            current_type_minus_total += diamond_count
+            desc_detail.put(type, current_type_minus_total)
+        }
+
+        def total = inc_total - desc_total
+        def myId = "${YMD}_diamond_dailyReport_stat".toString()
+
+        def row = $$('_id': myId, 'inc_total': inc_total, 'desc_total': desc_total, 'total': total, 'timestamp': gteMill,
+                'inc_detail': inc_detail, 'desc_detail': desc_detail, 'begin_surplus': begin_surplus, 'end_surplus': total)
+        diamond_dailyReport_stat.save($$(row))
+    }
+
+    /**
+     * 日期末结余
+     * @param begin
+     * @return
+     */
+    static Long lastDaySurplus(Long begin) {
+        long yesterDay = begin - DAY_MILLON
+        String ymd = new Date(yesterDay).format("yyyyMMdd")
+        def last_day = diamond_dailyReport_stat.findOne($$(_id: "${ymd}_diamond_dailyReport_stat".toString()))
+        println("last_day is ${last_day}")
+        return (last_day?.get('end_surplus') ?: 0) as Long;
     }
 
     static int day = 30
+
     static void main(String[] args) {
-        // 充值统计recovery
         long l = System.currentTimeMillis()
-//        //01.送礼日报表
         long begin = l
+
+        // 充值统计recovery
         pay_statics_recovery(day)
         println "${new Date().format('yyyy-MM-dd HH:mm:ss')}   pay_statics_recovery, cost  ${System.currentTimeMillis() - l} ms"
         Thread.sleep(1000L)
+
+
+        // 钻石出入统计
+        diamond_statics_recovery(day)
+        println "${new Date().format('yyyy-MM-dd HH:mm:ss')}   diamond_statics_recovery, cost  ${System.currentTimeMillis() - l} ms"
+        Thread.sleep(1000L)
+
 
         //落地定时执行的日志
         jobFinish(begin)
