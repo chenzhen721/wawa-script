c235d3b30eae2f3e74549bf9b6ee76a987cbb88a
jiao.li <jiao.li@2339.com>
2017-07-10 17:47:40 +0800

    update user live

--- src/main/groovy/crontab/st/UpdateUserAndLive.groovy
+++ src/main/groovy/crontab/st/UpdateUserAndLive.groovy
@@ -86,10 +86,6 @@ class UpdateUserAndLive {
     static main(arg) {
         final UpdateUserAndLive task = new UpdateUserAndLive()
 
-        long l = System.currentTimeMillis()
-        task.awardCoin()
-        println "${new Date().format('yyyy-MM-dd HH:mm:ss')}   awardCoin, cost  ${System.currentTimeMillis() - l} ms"
-
         //房间在线人数统计
         long begin = l
         Integer i = task.roomUserCount()
@@ -356,99 +352,6 @@ class UpdateUserAndLive {
         return jsonText;
     }
 
-
-
-    static final long[] family_level = [0,
-                                        20000,
-                                        80000,
-                                        180000,
-                                        320000,
-                                        500000,
-                                        720000,
-                                        980000,
-                                        1280000,
-                                        1620000,
-                                        2000000,
-                                        2420000,
-                                        2880000,
-                                        3380000,
-                                        3920000,
-                                        4500000,
-                                        5120000,
-                                        5780000,
-                                        6480000,
-                                        7220000,
-                                        8000000,
-                                        8820000,
-                                        9680000,
-                                        10580000,
-                                        11520000,
-                                        12500000,
-                                        13520000,
-                                        14580000,
-                                        15680000,
-                                        16820000,
-                                        18000000,
-                                        19220000,
-                                        20480000,
-                                        21780000,
-                                        23120000,
-                                        24500000,
-                                        25920000,
-                                        27380000,
-                                        28880000,
-                                        30420000,
-                                        32000000,
-                                        33620000,
-                                        35280000,
-                                        36980000,
-                                        38720000,
-                                        40500000,
-                                        42320000,
-                                        44180000,
-                                        46080000,
-                                        48020000,
-                                        50000000
-    ]
-
-    private static final int max_family_level = family_level.length;
-
-    public int familyLevel(long prestige) {
-        for (int i = 1; i < max_family_level; i++) {
-            if (prestige < (family_level[i])) {
-                return i - 1;
-            }
-        }
-        return max_family_level - 1;
-    }
-
-    void awardCoin(){
-        Random random = new Random()
-        def list = familyDB.find()
-        list.each {
-            BasicDBObject obj ->
-                Integer coin
-                def prestige = obj.containsField('prestige') ? obj['prestige'] as Long : 0L
-                def id = obj['_id'] as Integer
-                def level = familyLevel(prestige)
-                if(level >= 0 && level <=10){
-                    coin = random.nextInt(5000) + 10001
-                }else if(level >=11 && level <=20){
-                    coin = random.nextInt(7500) + 15001
-                }else if(level >=21 && level <=30){
-                    coin = random.nextInt(10000) + 20001
-                }else if (level >=31 && level <=40){
-                    coin = random.nextInt(12500) + 25001
-                }else if(level >=41 && level <=50){
-                    coin = random.nextInt(15000) + 30001
-                }else{
-                    coin = random.nextInt(17500) + 35001
-                }
-                coin = coin/60
-                familyDB.update($$('_id':id,gold:[$lte:1000000]),$$('$inc':['gold':coin]))
-                familyDB.update($$('_id':id,prestige:[$gt:0]),$$('$inc':['gold':coin]))
-        }
-    }
 
     /**
      * 用户封禁到期自动解封
