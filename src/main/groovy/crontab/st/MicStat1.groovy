515cb5444b452215cc42d6ab4cd8c980252283d9
zhen.chen <zhen.chen@2339.com>
2017-11-20 14:48:59 +0800

    crontab

--- src/main/groovy/crontab/st/MicStat1.groovy
+++ src/main/groovy/crontab/st/MicStat1.groovy
@@ -109,7 +109,7 @@ class MicStat1 {
 
         //设置超时、概率
         def catch_room = mongo.getDB('xy_lab').getCollection('catch_room')
-        catch_room.find($$(playtime: [$ne: 40], winrate: [$ne: 25])).limit(2).toArray().each {BasicDBObject obj ->
+        catch_room.find($$(online: true)).limit(2).toArray().each {BasicDBObject obj ->
             if (!setProbAndtime(obj['fid'] as String, 25, 40)) {
                 println obj['_id']
             } else {
