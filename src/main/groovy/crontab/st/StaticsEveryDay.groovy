9b7254f1796949b9c6455a37cf6f664e80cf3fab
zhen.chen <zhen.chen@2339.com>
2017-12-01 15:18:16 +0800

    crontab

--- src/main/groovy/crontab/st/StaticsEveryDay.groovy
+++ src/main/groovy/crontab/st/StaticsEveryDay.groovy
@@ -293,7 +293,7 @@ class StaticsEveryDay {
                 ), true, false)
     }
 
-    //次日留存
+    //todo check次日留存
     static payStaticsRetation(int i) {
         //记录在前一日的report中
         def stat_report = mongo.getDB('xy_admin').getCollection('stat_report')
