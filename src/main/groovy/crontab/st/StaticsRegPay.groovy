ec7f970bb76382521cf4b8fef403634a7b889ad4
zhen.chen <zhen.chen@2339.com>
2018-01-12 16:42:13 +0800

    crontab

--- src/main/groovy/crontab/st/StaticsRegPay.groovy
+++ src/main/groovy/crontab/st/StaticsRegPay.groovy
@@ -45,6 +45,8 @@ class StaticsRegPay {
     static DBCollection diamond_add_logs = mongo.getDB("xylog").getCollection("diamond_add_logs")
     static DBCollection diamond_cost_logs = mongo.getDB("xylog").getCollection("diamond_cost_logs")
     static DBCollection apply_post_logs = mongo.getDB("xylog").getCollection("apply_post_logs")
+    static DBCollection catch_record = mongo.getDB("xy_catch").getCollection("catch_record")
+    static DBCollection catch_toy = mongo.getDB("xy_catch").getCollection("catch_toy")
 
     /**
      * regs:[] //注册IDs
@@ -403,9 +405,14 @@ class StaticsRegPay {
             goods_count = goods_count + count
             postage = postage + cost(count)
             toys.each {BasicDBObject toy ->
-                def toy_cost = toy['cost'] as Integer
+                def record_id = toy['record_id'] as Integer
+                def record = catch_record.findOne($$(_id: record_id), $$(cost: 1))
+                if (record != null) {
+                    goods_cost = goods_cost + (record['cost'] as Integer)
+                }
+                def toy_cost = catch_toy.findOne($$(_id: toy['_id'] as Integer), $$(cost: 1))
                 if (toy_cost != null) {
-                    goods_cost = goods_cost + toy_cost
+                    total_cost = total_cost + toy_cost
                 }
             }
             uids.add(obj['user_id'] as Integer)
@@ -427,7 +434,7 @@ class StaticsRegPay {
         if (count >= 3 && count <= 6) {
             return 7
         }
-        if (count >= 6 && count <= 9) {
+        if (count > 6 && count <= 9) {
             return 14
         }
         if (count > 9) {
