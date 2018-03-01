5444d1c5d03fe071f0475af0aadd0bab22ab089a
zhen.chen <zhen.chen@2339.com>
2018-03-01 14:25:35 +0800

    每小时统计一次

--- src/main/groovy/crontab/st/StaticsEveryHour.groovy
+++ src/main/groovy/crontab/st/StaticsEveryHour.groovy
@@ -100,6 +100,9 @@ class StaticsEveryHour {
         if (i != 0) {
             cal.add(Calendar.HOUR_OF_DAY, i)
         }
+        cal.set(Calendar.MINUTE, 0)
+        cal.set(Calendar.SECOND, 0)
+        cal.set(Calendar.MILLISECOND, 0)
         cal.getTime().getTime()
     }
 
